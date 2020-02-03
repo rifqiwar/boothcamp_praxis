fd41de3ed70499be0289b43dae053527634e6b06
Rifqiwar <r.wariyanto@gmail.com>
2020-02-03 13:50:39 +0700

    ubah dulu

---
+++ kasus/kasus_mystagehand/fancy_project/example/fancy_project_example.dart
@@ -0,0 +1,6 @@
+import 'package:fancy_project/fancy_project.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ kasus/kasus_mystagehand/fancy_project/lib/fancy_project.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library fancy_project;
+
+export 'src/fancy_project_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
